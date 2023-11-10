00fd7c2cbf91a4a2ebc6ab0af2346ef846c6c6f5
Arabinda Samal <arabinda2919@gmail.com>
2023-11-10 16:23:41 +0530

    railway updates

--- portal/settings.py
+++ portal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJNAGO_SECURITY_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
